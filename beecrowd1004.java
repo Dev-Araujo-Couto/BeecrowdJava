19f2706ee47023e52a4ec5f90bff9c046c4abae5
Vinicius Araujo <viniacouto@live.com>
2026-04-13 19:05:03 -0300

    Add files via upload

---
+++ beecrowd1004.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+public class beecrowd1004 {
+        public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int A,B,X;
+        A = sc.nextInt();
+        B = sc.nextInt();
+        X = A * B;
+        System.out.println("PROD = " + X);
+        sc.close();
+
+    }
+    
+}
